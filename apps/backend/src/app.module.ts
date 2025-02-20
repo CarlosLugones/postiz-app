7ad339c0c087d7f1e120493860a73180b5b3f6ff
Carlos Lugones <contact@carloslugones.com>
2025-02-20 23:17:52 +0100

    remove: ThrottlerModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -8,7 +8,6 @@ import { BullMqModule } from '@gitroom/nestjs-libraries/bull-mq-transport-new/bu
 import { PluginModule } from '@gitroom/plugins/plugin.module';
 import { PublicApiModule } from '@gitroom/backend/public-api/public.api.module';
 import { ThrottlerBehindProxyGuard } from '@gitroom/nestjs-libraries/throttler/throttler.provider';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { AgentModule } from '@gitroom/nestjs-libraries/agent/agent.module';
 
 @Global()
@@ -20,12 +19,6 @@ import { AgentModule } from '@gitroom/nestjs-libraries/agent/agent.module';
     PluginModule,
     PublicApiModule,
     AgentModule,
-    ThrottlerModule.forRoot([
-      {
-        ttl: 3600000,
-        limit: 30,
-      },
-    ]),
   ],
   controllers: [],
   providers: [
